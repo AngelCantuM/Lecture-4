a5309fec88fae10f8e4dea2dcca9751c4c2d695d
AngelCantuM <100724032+AngelCantuM@users.noreply.github.com>
2022-03-14 14:03:11 -0700

    Update javapart.js

--- javapart.js
+++ javapart.js
@@ -2,4 +2,4 @@ var cost1 = 34;
 var cost2 = 35;
 var score = cost1 + cost2;
 document.getElementById("demo").innerHTML =
-" total of " + score + "however my grade in this assignment should be a 100/100"
+" total of " + score 
